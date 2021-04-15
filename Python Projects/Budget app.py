2dbe8bced6393d4124a10a8fc0cd0975b5e572a6
ianbeltrao <ianbeltrao@gmail.com>
2021-04-15 14:48:10 -0300

    final

--- Python Projects/Budget app.py
+++ Python Projects/Budget app.py
@@ -1,33 +1,105 @@
-import re
-
 class Category:
+
     def __init__(self, name):
         self.name = name 
         self.ledger = list()
 
-    def deposit(amount, description=""):
+    def __str__(self):
+        title = f"{self.name:*^30}\n"
+        items = ""
+        total = 0
+
+        for item in self.ledger:
+            items += f"{item['description'][0:23]:23}" + f"{item['amount']:>7.2f}" + "\n" 
+            total += item["amount"]
+
+        output = title + items + f"Total: {total}"
+        return output
+
+
+    def deposit(self, amount, description=""):
         self.ledger.append({"amount": amount, "description": description})
 
-    def withdraw(amount, description=""):
+    def withdraw(self, amount, description=""):
         if (self.check_funds(amount)):
             self.ledger.append({"amount": -amount, "description": description})
             return True
         return False
 
-    def get_balance():
+    def get_balance(self):
         balance = 0
         for transaction in self.ledger:
-            splitted = re.split(", ", transaction)
-            for chunk in splitted:
-                if type(chunk) == int or type(chunk) == float:
-                    balance += chunk
+            balance += transaction.get("amount")
+        return balance
+
+    def transfer(self, amount, category):
+        if (self.check_funds(amount)):
+            self.withdraw(amount, f"Transfer to {category.name}")
+            category.deposit(amount, f"Transfer from {category.name}")
+            return True
+        return False
+
+    def check_funds(self, amount):
+        if amount > self.get_balance():
+            return False
+        return True
+
+#This part is a really though one and i dont think we have the knowledge to write this from the FCC Python Course.
+#This section has some code that is not mine.
+def create_spend_chart(categories):
+  category_names = []
+  spent = []
+  spent_percentages = []
+
+  for category in categories:
+    total = 0
+    for item in category.ledger:
+      if item['amount'] < 0:
+        total -= item['amount']
+    spent.append(round(total, 2))
+    category_names.append(category.name)
+
+  for amount in spent:
+    spent_percentages.append(round(amount / sum(spent), 2)*100)
+
+  graph = "Percentage spent by category\n"
+
+  labels = range(100, -10, -10)
+
+  for label in labels:
+    graph += str(label).rjust(3) + "| "
+    for percent in spent_percentages:
+      if percent >= label:
+        graph += "o  "
+      else:
+        graph += "   "
+    graph += "\n"
+
+  graph += "    ----" + ("---" * (len(category_names) - 1))
+  graph += "\n     "
+
+  longest_name_length = 0
+
+  for name in category_names:
+    if longest_name_length < len(name):
+      longest_name_length = len(name)
+
+  for i in range(longest_name_length):
+    for name in category_names:
+      if len(name) > i:
+        graph += name[i] + "  "
+      else:
+        graph += "   "
+    if i < longest_name_length-1:
+      graph += "\n     "
+
+    
+
+  return(graph)
 
 
-food = Category("Food")
-food.deposit(1000)
-food.withdraw(10.15, "groceries")
-food.withdraw(15.89, "restaurant and more food for dessert")
-print(food.get_balance())
+
+
 
 
 
